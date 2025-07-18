36ffc8346071a66df16854602f84cae460c928dd
FPabloA <pabloayala@protonmail.com>
2025-07-18 12:00:32 -0500

    finished implementing basic functionality of the etch-a-sketch project

--- javascript.js
+++ javascript.js
@@ -1,15 +1,35 @@
-const GRIDWIDTH = 18
-const GRIDHEIGHT = 18
+let GRIDSIZE = 16
+
 const gridWrapper = document.querySelector("#grid-wrapper");
+const gridButton = document.querySelector("#grid-button");
+
+gridButton.onclick = () => {
+    let size = parseInt(prompt("Enter the size of the grid. (MAX of 100)"));
+
+
+    if (Number.isInteger(size) && size < 100){
+        while (gridWrapper.firstChild) {
+            gridWrapper.removeChild(gridWrapper.firstChild);
+        }
+        GRIDSIZE = size;
+    }
+    console.log(GRIDSIZE);
+    fillGrid();
+};
 
 function fillGrid() {
-    for(let i = 0; i < GRIDHEIGHT; i++){
+    for(let i = 0; i < GRIDSIZE; i++){
         const gridRow = document.createElement("div");
         gridRow.className = "grid-row"
         gridWrapper.appendChild(gridRow)
-        for(let j = 0; j < GRIDWIDTH; j++){
+        for(let j = 0; j < GRIDSIZE; j++){
             const gridDiv = document.createElement("div");
             gridDiv.className = "grid-square"
+
+            gridDiv.addEventListener('mouseover', function() {
+                this.style.cssText = "background-color: gray;"
+            })
+
             gridRow.appendChild(gridDiv);
         }
     }
